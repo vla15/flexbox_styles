ac0830540d5350c6e171cd2e5f3dacad8c5fffcb
Vincent La <vincela15@gmail.com>
2017-09-09 13:09:03 -0700

    building out html component class and flex nav class

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,43 @@
-const date = document.querySelector('.date');
-console.dir(date);
-date.textContent = new Date().toDateString();
+var menu = document.querySelector('.nav__burger');
+console.dir(menu);
+
+
+class HtmlComponents {
+  constructor(selector) {
+    this.baseClass = 'component';
+    this.el = document.querySelector(selector);
+  }
+  getElement(name) {
+    return this.el.querySelector(`.${this.baseClass}__${name}`)
+  }
+  addModifier(modifier) {
+    this.el.classList.add(`${this.baseClass}--${modifier}`);
+  }
+  removeModifier(modifier) {
+    this.el.classList.remove(`${this.baseClass}--${modifier}`);
+  }
+}
+
+class FlexNav extends HtmlComponents {
+  constructor(selector) {
+    super(selector);
+    this.baseClass = 'nav';
+    this.isOpen = false;
+
+    this.burger = this.getElement('burger');
+
+    this.burger.addEventListener('click', (e) => { this.open(e) });
+    this.burger.addEventListener('touchend', (e) => { this.open(e) });
+  }
+
+  open(event) {
+    event.preventDefault();
+    this.isOpen = !this.isOpen;
+
+    if (this.isOpen) {
+      this.burger.addModifier('opened');
+    } else {
+      this.burger.removeModifier('opened');
+    }
+  }
+}
